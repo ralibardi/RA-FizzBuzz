5c9dac9c7850a44babc1d55688091b7e3861fd1b
Ronny Alibardi <alexandro_giora@outlook.com>
2023-11-16 15:39:01 +0000

    Add comment

--- RA-FizzBuzz/Configuration/ServiceCollectionExtensions.cs
+++ RA-FizzBuzz/Configuration/ServiceCollectionExtensions.cs
@@ -12,6 +12,8 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection ConfigureDependencies(this IServiceCollection services)
     {
         services.AddSingleton<IFizzBuzz, FizzBuzz>();
+
+        // Rules
         services.AddSingleton<IFizzBuzzRule, FizzRule>();
         services.AddSingleton<IFizzBuzzRule, BuzzRule>();
         services.AddSingleton<IFizzBuzzRule, FizzBuzzRule>();
