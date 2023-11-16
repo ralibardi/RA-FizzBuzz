2b7a535568305ec9ba0e7548c2a99f28c1b491e8
Ronny Alibardi <alexandro_giora@outlook.com>
2023-11-16 15:04:13 +0000

    More tests

--- RA-FizzBuzz.Tests/FizzBuzzTests.cs
+++ RA-FizzBuzz.Tests/FizzBuzzTests.cs
@@ -9,4 +9,18 @@ public class FizzBuzzTests
         var rule = new FizzRule();
         Assert.AreEqual("Fizz", rule.ApplyRule(3));
     }
+
+    [Test]
+    public void FizzRule_ShouldReturnBuzz()
+    {
+        var rule = new BuzzRule();
+        Assert.AreEqual("Buzz", rule.ApplyRule(5));
+    }
+
+    [Test]
+    public void FizzRule_ShouldReturnFizzBuzz()
+    {
+        var rule = new FizzBuzzRule();
+        Assert.AreEqual("FizzBuzz", rule.ApplyRule(15));
+    }
 }
